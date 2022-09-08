55c2ef7c7e91c6452d0f20b0c4055b44242551aa
Tuấn Vũ Anh <tuancongtuyn@gmail.com>
2022-09-08 10:33:39 +0700

    comments: note service

--- src/main/java/com/vmodev/tuanva/notes/service/note/NoteService.java
+++ src/main/java/com/vmodev/tuanva/notes/service/note/NoteService.java
@@ -5,12 +5,38 @@ import com.vmodev.tuanva.notes.dto.NoteRequestDTO;
 import com.vmodev.tuanva.notes.model.PlainNote;
 
 public interface NoteService {
+
+	/**
+	 * Creates the Note.
+	 *
+	 * @param request the request
+	 * @return true, if successful
+	 * @throws Exception the exception
+	 */
 	boolean create(NoteRequestDTO request) throws Exception;
 
+	/**
+	 * Delete note by it's id.
+	 *
+	 * @param id the id
+	 * @return true, if successful
+	 */
 	boolean delete(Long id);
 
+	/**
+	 * Update note.
+	 *
+	 * @param request the request
+	 * @return the base response DTO
+	 */
 	BaseResponseDTO<PlainNote> update(NoteRequestDTO request);
 
+	/**
+	 * Gets the note by id.
+	 *
+	 * @param id the id
+	 * @return the base response DTO
+	 */
 	BaseResponseDTO<PlainNote> get(Long id);
 
 }
